db95154f23b0b72dc4f236326a2fa81962d18dfe
RitikPadhy <ritipkp2002@gmail.com>
2024-05-05 17:05:55 +0530

    Change for 60 enteries

--- satellite_tracking_system/satellite_tracker/management/commands/populate_satellites.py
+++ satellite_tracking_system/satellite_tracker/management/commands/populate_satellites.py
@@ -12,30 +12,39 @@ class Command(BaseCommand):
         
         if response.status_code == 200:
             json_data = response.json()
-            satellites_data = json_data
-
-            satellites_to_create = []
+            satellites_data = json_data[:10]
+            sat_obj = []
             for satellite_data in satellites_data:
-                satellite = Satellite(
-                    object_name=satellite_data['OBJECT_NAME'],
-                    object_id=satellite_data['OBJECT_ID'],
-                    epoch=satellite_data['EPOCH'],
-                    mean_motion=satellite_data['MEAN_MOTION'],
-                    eccentricity=satellite_data['ECCENTRICITY'],
-                    inclination=satellite_data['INCLINATION'],
-                    ra_of_asc_node=satellite_data['RA_OF_ASC_NODE'],
-                    arg_of_pericenter=satellite_data['ARG_OF_PERICENTER'],
-                    mean_anomaly=satellite_data['MEAN_ANOMALY'],
-                    ephemeris_type=satellite_data['EPHEMERIS_TYPE'],
-                    classification_type=satellite_data['CLASSIFICATION_TYPE'],
-                    norad_cat_id=satellite_data['NORAD_CAT_ID'],
-                    element_set_no=satellite_data['ELEMENT_SET_NO'],
-                    rev_at_epoch=satellite_data['REV_AT_EPOCH'],
-                    bstar=satellite_data['BSTAR'],
-                    mean_motion_dot=satellite_data['MEAN_MOTION_DOT'],
-                    mean_motion_ddot=satellite_data['MEAN_MOTION_DDOT']
-                )
-                satellites_to_create.append(satellite)
+                sat_obj.append(satellite_data['OBJECT_ID'][:-1])
+                
+            satellites_to_create = []
+
+            for i in sat_obj:
+                new_url="https://celestrak.org/NORAD/elements/gp.php?INTDES="+i+"&FORMAT=json"
+                new_response = requests.get(new_url)
+                sat_resp = new_response.json()             
+                   
+                for satellite_data in sat_resp:
+                    satellite = Satellite(
+                        object_name=satellite_data['OBJECT_NAME'],
+                        object_id=satellite_data['OBJECT_ID'],
+                        epoch=satellite_data['EPOCH'],
+                        mean_motion=satellite_data['MEAN_MOTION'],
+                        eccentricity=satellite_data['ECCENTRICITY'],
+                        inclination=satellite_data['INCLINATION'],
+                        ra_of_asc_node=satellite_data['RA_OF_ASC_NODE'],
+                        arg_of_pericenter=satellite_data['ARG_OF_PERICENTER'],
+                        mean_anomaly=satellite_data['MEAN_ANOMALY'],
+                        ephemeris_type=satellite_data['EPHEMERIS_TYPE'],
+                        classification_type=satellite_data['CLASSIFICATION_TYPE'],
+                        norad_cat_id=satellite_data['NORAD_CAT_ID'],
+                        element_set_no=satellite_data['ELEMENT_SET_NO'],
+                        rev_at_epoch=satellite_data['REV_AT_EPOCH'],
+                        bstar=satellite_data['BSTAR'],
+                        mean_motion_dot=satellite_data['MEAN_MOTION_DOT'],
+                        mean_motion_ddot=satellite_data['MEAN_MOTION_DDOT']
+                    )
+                    satellites_to_create.append(satellite)
 
             Satellite.objects.bulk_create(satellites_to_create)
             self.stdout.write(self.style.SUCCESS('Satellites table populated successfully.'))
